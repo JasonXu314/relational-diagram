0310efff8e6be2ad4477088813bdbc76f1ba7e1e
Jason Xu <Xinwen31415@gmail.com>
2023-07-09 13:02:21 -0500

    added site title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import { SvelteUIProvider } from '@svelteuidev/core';
 </script>
 
+<svelte:head>
+	<title>Relational Diagram</title>
+</svelte:head>
+
 <SvelteUIProvider withNormalizeCSS withGlobalStyles>
 	<slot />
 </SvelteUIProvider>
